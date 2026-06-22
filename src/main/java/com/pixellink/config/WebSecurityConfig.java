a901aa4da1dfc9b251b8dd3c066735ed8f246a6e
이영현 <hong.git@yahoo.com>
2026-06-23 01:32:15 +0900

    feat: add sitemap.xml and robots.txt for search engine optimization

--- src/main/java/com/pixellink/config/WebSecurityConfig.java
+++ src/main/java/com/pixellink/config/WebSecurityConfig.java
@@ -65,7 +65,9 @@ public class WebSecurityConfig {
                     "/api/links/*/payments/confirm",
                     "/info/faq",
                     "/info/privacy",
-                    "/info/terms"
+                    "/info/terms",
+                    "/sitemap.xml",
+                    "/robots.txt"
                 ).permitAll()
                 // 로그인, 회원가입 및 설치 페이지는 누구나 접근 가능 (보호막에 걸리기 전에 통과)
                 .requestMatchers("/app/login", "/app/signup", "/app/signup/process", "/app/install", "/app/install/process").permitAll()
